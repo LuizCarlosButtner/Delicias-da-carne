89cd39c9577805f8726716f786ce61ff7deab08c
LuiCarlosButtner <udroglot@gmail.com>
2023-03-14 14:15:55 -0300

    alignment of elements, creating a branch for building the carousel

---
+++ script.js
@@ -0,0 +1,16 @@
+const celulas = document.getElementById("container")
+const celula = document.querySelectorAll(".celula")
+
+let idx = 0 ;
+
+let carrossel =()=>{
+    ind++;
+
+    if(idx > celula.length - 1){
+        idx = 0;
+    }
+
+    celula.style.transform - `translateX(${-idx * 275}px)`
+}
+
+setInterval(carrossel, 1800);
