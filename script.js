c0dca2f04f599836a250646ef4c2806d9f3cf0f6
LuiCarlosButtner <udroglot@gmail.com>
2023-03-14 16:57:30 -0300

    I made a new branch to implement the carousel

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 const celulas = document.getElementById("container")
-const celula = document.querySelectorAll(".celula")
+const celula = document.querySelectorAll(".container .celula")
+
+
 
 let idx = 0 ;
 
 let carrossel =()=>{
-    ind++;
+    idx++;
 
-    if(idx > celula.length - 1){
-        idx = 0;
-    }
+    if(idx > 10) idx = 0;
 
-    celula.style.transform - `translateX(${-idx * 275}px)`
+    celulas.style.transform = `translateX(${-idx * 314}px)`;
+    console.log(idx)
 }
 
-setInterval(carrossel, 1800);
+setInterval(carrossel, 2000);
+
+   
+
+
+
+const celulas2 = document.getElementById("container2")
+const celula2 = document.querySelectorAll(".container .celula")
+
+
+
+
+let carrossel2 =()=>{
+    idx++;
+
+    if(idx > 10) idx = 0;
+
+    celulas2.style.transform = `translateX(${-idx * 314}px)`;
+    console.log(idx)
+}
+
+setInterval(carrossel2, 2000);
+
+
