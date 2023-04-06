1fa740b59d8dd15285cdefcb234413959987cbc6
LuizCarlosButtner <udroglot@gmail.com>
2023-04-06 18:09:13 -0300

    I put Qr codes that appear in the mouseenter of the social network logo

---
+++ script.js
@@ -0,0 +1,13 @@
+// let qr1 = document.getElementById("teste1")
+// let logoGit = document.getElementById("logoGit")
+
+// let qRcode1 = document.getElementById("qRcode1")
+// let qRcode2 = document.getElementById("qRcode2")
+// let qRcode3 = document.getElementById("qRcode3")
+
+function aparecerQr(param){
+    document.getElementById(param).classList.add("aparecer")
+}
+function desaparecerQr(param){
+    document.getElementById(param).classList.remove("aparecer")
+}
